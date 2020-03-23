b256ef2bee1893083c22878eb7175066cf9cb873
kakanishi <kenneth.t.miura@gmail.com>
2020-03-23 11:21:15 -0400

    Removed Hardcoding. Now generalizeable

--- syncer.py
+++ syncer.py
@@ -6,6 +6,7 @@ import re
 import argparse
 
 
+# TODO: alter so this can take N directories & targets
 def set_up_drive(settings_file, credentials_file):
     print(
         f'Settings File Location: {settings_file} Credentials File Location: {credentials_file}'
@@ -31,17 +32,17 @@ def set_up_drive(settings_file, credentials_file):
     return GoogleDrive(gauth)
 
 
-def get_drive_target_dir_id(drive, backup_folder):
+def get_drive_target_dir_id(drive, drive_folder):
     folder_query = "'root' in parents and trashed=false and mimeType='application/vnd.google-apps.folder'"
     folder_list = drive.ListFile({"q": folder_query}).GetList()
     for folder in folder_list:
-        if folder['title'] == backup_folder:
+        if folder['title'] == drive_folder:
             drive_target_dir_id = folder['id']
     return drive_target_dir_id
 
 
-def backup_files(drive, backup_folder):
-    drive_target_dir_id = get_drive_target_dir_id(drive, backup_folder)
+def backup_dir(drive, drive_folder, folder_to_sync):
+    drive_target_dir_id = get_drive_target_dir_id(drive, drive_folder)
     # Clean all old files in target
     query = f"'{drive_target_dir_id}' in parents and trashed=false"
     file_list = drive.ListFile({'q': query}).GetList()
@@ -49,17 +50,12 @@ def backup_files(drive, backup_folder):
         file1.Delete()
 
     # Upload local files to drive
-    sync_dir_path = os.path.join(os.path.expanduser('~'), 'Google-Drive',
-                                 '_notebook')
-    for filename in glob.glob(os.path.join(sync_dir_path, '*.md')):
-        # Removing first 10 characters in reg ex to get rid of the _notebook\
-        name_without_path = re.search(r"_notebook[^.]+\.\w+",
-                                      filename).group()[10:]
-
-        print(name_without_path)
+    for file_path in glob.glob(os.path.join(folder_to_sync, '*')):
+        file_name = re.split('/', file_path)[-1]
+        print(file_name)
         g_file = drive.CreateFile({"parents": [{"id": drive_target_dir_id}]})
-        g_file.SetContentFile(filename)
-        g_file['title'] = name_without_path
+        g_file.SetContentFile(file_path)
+        g_file['title'] = file_name
         g_file.Upload()
 
 
@@ -73,12 +69,14 @@ def main():
                         nargs='?',
                         default='credentials.txt',
                         help="location of the credentials.txt file")
-    parser.add_argument("backup_folder")
+    parser.add_argument("drive_folder", help="Path to Folder in Drive that you want to backup to")
+    parser.add_argument("folder_to_sync", help="Path to Local Folder you want to sync")
     settings_file = parser.parse_args().setting_file
     credentials_file = parser.parse_args().credentials_file
-    backup_folder = parser.parse_args().backup_folder
+    drive_folder = parser.parse_args().drive_folder
+    folder_to_sync = parser.parse_args().folder_to_sync
     drive = set_up_drive(settings_file, credentials_file)
-    backup_files(drive, backup_folder)
+    backup_dir(drive, drive_folder, folder_to_sync)
 
 
 if __name__ == '__main__':
